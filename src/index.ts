42ef635fb590e554e2c11565ce3255ed7ce3e2a6
Derrick Hammer <derrick@derrickhammer.com>
2023-08-31 07:22:16 -0400

    fix: add createKeyPair factory that defaults to a random key

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { S5Node } from "#node.js";
 import type { S5NodeConfig } from "#node.js";
 import type { SignedRegistryEntry } from "#service/registry.js";
+import { ed25519 } from "@noble/curves/ed25519";
+import KeyPairEd25519 from "#ed25519.js";
 
 export * from "./types.js";
 export {
@@ -12,4 +14,8 @@ export type { S5NodeConfig, SignedRegistryEntry };
 
 export function createNode(config: S5NodeConfig) {
   return new S5Node(config);
+}
+
+export function createKeyPair(privateKey?: Uint8Array) {
+  return new KeyPairEd25519(privateKey ?? ed25519.utils.randomPrivateKey());
 }
