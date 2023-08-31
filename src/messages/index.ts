87d1e6b0b4f4b7ec7ae9af85b9afae5c4eaa8630
Derrick Hammer <derrick@derrickhammer.com>
2023-08-31 06:54:16 -0400

    fix: update registered messages

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,9 +1,24 @@
 import { P2PMessageHandler } from "#types.js";
 import handshakeOpen from "#messages/handshakeOpen.js";
+import {
+  protocolMethodHandshakeOpen,
+  protocolMethodRegistryQuery,
+  protocolMethodSignedMessage,
+  recordTypeRegistryEntry,
+  recordTypeStorageLocation,
+} from "#constants.js";
+import registryQuery from "#messages/registryQuery.js";
+import registryEntry from "#messages/registryEntry.js";
+import storageLocation from "#messages/storageLocation.js";
+import signedMessage from "#messages/signedMessage.js";
 
 const messages = new Map<number, P2PMessageHandler>(
   Object.entries({
-    protocolMethodHandshakeOpen: handshakeOpen,
+    [protocolMethodHandshakeOpen]: handshakeOpen,
+    [protocolMethodRegistryQuery]: registryQuery,
+    [recordTypeRegistryEntry]: registryEntry,
+    [recordTypeStorageLocation]: storageLocation,
+    [protocolMethodSignedMessage]: signedMessage,
   }).map(([key, value]) => [Number(key), value]),
 );
 
--- src/messages/signedMessages/index.ts
+++ src/messages/signedMessages/index.ts
@@ -1,9 +1,15 @@
 import { P2PSignedMessageHandler } from "#types.js";
 import handshakeDone from "#messages/signedMessages/handshakeDone.js";
+import {
+  protocolMethodAnnouncePeers,
+  protocolMethodHandshakeDone,
+} from "#constants.js";
+import announcePeers from "#messages/signedMessages/announcePeers.js";
 
 const messages = new Map<number, P2PSignedMessageHandler>(
   Object.entries({
-    protocolMethodHandshakeOpen: handshakeDone,
+    [protocolMethodHandshakeDone]: handshakeDone,
+    [protocolMethodAnnouncePeers]: announcePeers,
   }).map(([key, value]) => [Number(key), value]),
 );
 
