5b471ea9a7659da65b889a5e8ae7791f9f77f7ef
Derrick Hammer <derrick@derrickhammer.com>
2023-09-06 07:34:22 -0400

    feat: initial version

---
+++ docusaurus.config.js
@@ -0,0 +1,111 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Lume Web Docs",
+  tagline: "Freedom. Privacy. Ownership",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.lumeweb.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://git.lumeweb.com/LumeWeb/docs.lumeweb.com/src/branch/master/",
+        },
+        blog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      navbar: {
+        title: "Lume Web",
+        logo: {
+          alt: "Lume Web",
+          src: "img/logo.svg",
+          srcDark: "img/logo_dark.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Lume Web",
+                href: "https://lumeweb.com",
+              },
+              {
+                label: "Discord",
+                href: "https://discord.gg/qpC8ADp3rS",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/lumeweb3",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/lumeweb3",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Hammer Technologies LLC. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
