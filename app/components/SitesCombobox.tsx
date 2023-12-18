e18c76ca8b2d81867ace60fc25d5d8b4e6288eae
Derrick Hammer <derrick@derrickhammer.com>
2023-12-17 22:39:09 -0500

    refactor: swf not needed for now

--- app/components/SitesCombobox.tsx
+++ app/components/SitesCombobox.tsx
@@ -1,39 +1,39 @@
-"use client"
+import * as React from "react";
 
-import * as React from "react"
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-  CommandList
-} from "@/components/ui/command"
+  CommandList,
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
-  PopoverTrigger
-} from "@/components/ui/popover"
-import { ChevronDownIcon } from "@heroicons/react/24/solid"
-import useSWR from "swr"
-import { getAvailableSites } from "@/utils"
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import useSWR from "swr";
+import { getAvailableSites } from "@/utils";
 
 export function SitesCombobox() {
-  const {data: statuses} = useSWR('/api/statuses', getAvailableSites)
-  const [open, setOpen] = React.useState(false)
-  const [selectedStatus, setSelectedStatus] = React.useState<SelectOptions | null>(
-    null
-  )
+  const { statuses } = getAvailableSites();
+  const [open, setOpen] = React.useState(false);
+  const [selectedStatus, setSelectedStatus] =
+    React.useState<SelectOptions | null>(null);
 
   return (
     <div className="flex flex- items-center space-x-4">
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant={"ghost"} className="max-w-[120px] focus:ring-2 focus:ring-ring px-2 font-bold items-center w-full flex justify-between text-white text-xs uppercase">
-              {selectedStatus ? <>{selectedStatus.label}</> : <>All Sites</>}
-              <ChevronDownIcon className="ml-3 w-5 h-5"/>
+          <Button
+            variant={"ghost"}
+            className="max-w-[120px] focus:ring-2 focus:ring-ring px-2 font-bold items-center w-full flex justify-between text-white text-xs uppercase"
+          >
+            {selectedStatus ? <>{selectedStatus.label}</> : <>All Sites</>}
+            <ChevronDownIcon className="ml-3 w-5 h-5" />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="p-0" side="right" align="start">
@@ -51,8 +51,8 @@ export function SitesCombobox() {
                       setSelectedStatus(
                         statuses.find((priority) => priority.value === value) ||
                           null
-                      )
-                      setOpen(false)
+                      );
+                      setOpen(false);
                     }}
                   >
                     {status.label}
@@ -64,5 +64,5 @@ export function SitesCombobox() {
         </PopoverContent>
       </Popover>
     </div>
-  )
+  );
 }
