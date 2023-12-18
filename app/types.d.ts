bb8fd35b49858571ccf43b247c6c55a95bbcb617
Derrick Hammer <derrick@derrickhammer.com>
2023-12-17 22:43:46 -0500

    fix: add missing types

---
+++ app/types.d.ts
@@ -0,0 +1,12 @@
+export type SearchResult = {
+  id: number;
+  timestamp: Date;
+  title: string;
+  description: string;
+  slug: string;
+};
+
+export type SelectOptions = {
+  value: string;
+  label: string;
+};
