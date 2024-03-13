3250edf1662f54a33b521b54abc9b0c1d2f37630
Derrick Hammer <derrick@derrickhammer.com>
2024-03-13 10:22:23 -0400

    chore: prettier

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,21 +1,21 @@
-import AccountApi from './account.js';
+import AccountApi from "./account.js";
 
 export class Sdk {
-    private apiUrl: string;
-    private accountApi?: AccountApi;
+  private apiUrl: string;
+  private accountApi?: AccountApi;
 
-    constructor(apiUrl: string) {
-        this.apiUrl = apiUrl;
-    }
+  constructor(apiUrl: string) {
+    this.apiUrl = apiUrl;
+  }
 
-    public static create(apiUrl: string): Sdk {
-        return new Sdk(apiUrl);
-    }
+  public static create(apiUrl: string): Sdk {
+    return new Sdk(apiUrl);
+  }
 
-    public account(): AccountApi {
-        if (!this.accountApi) {
-            this.accountApi = AccountApi.create(this.apiUrl);
-        }
-        return this.accountApi;
+  public account(): AccountApi {
+    if (!this.accountApi) {
+      this.accountApi = AccountApi.create(this.apiUrl);
     }
+    return this.accountApi;
+  }
 }
