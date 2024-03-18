7e78f17937719d49c0aeca8c2695df95e4fefe59
Derrick Hammer <derrick@derrickhammer.com>
2024-03-18 11:57:38 -0400

    ci: disable protocolImports

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             ignoredRouteFiles: ["**/*.css"]
         }),
         tsconfigPaths(),
-        nodePolyfills(),
+        nodePolyfills({protocolImports: false}),
     ],
     server: {
         fs: {
