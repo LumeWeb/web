14c854c45592b489265536693e8dbcfe618cd89d
Derrick Hammer <derrick@derrickhammer.com>
2024-03-18 10:46:45 -0400

    fix: update imports

--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,7 +1,7 @@
 import { Registry } from "./registry";
-import { createProtocol, Protocol } from "src/protocol/impl/protocol.js";
+import { createProtocol, Protocol } from "./impl/protocol.js";
 import { S5Client } from "@lumeweb/s5-js";
-import { PROTOCOL_S5, S5 } from "src/protocol/impl/s5.js";
+import { PROTOCOL_S5, S5 } from "./impl/s5.js";
 
 export function registerDefaults(registry: Registry) {
   registry.register<S5Client>(
