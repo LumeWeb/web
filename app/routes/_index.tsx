9a8a7ee97817621f7a4378b25ef9c8b048b81f99
Juan Di Toro <ditorojuanf@gmail.com>
2024-03-19 23:41:45 +0100

    fix: route redirection issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,19 @@
-import {useGo, useIsAuthenticated} from "@refinedev/core";
-import {useEffect} from "react";
+import { useGo, useIsAuthenticated } from "@refinedev/core";
 
 export default function Index() {
-    const {isLoading, data} = useIsAuthenticated();
+  const { isLoading, data } = useIsAuthenticated();
 
-    const go = useGo();
+  const go = useGo();
 
-    useEffect(() => {
-        if (!isLoading) {
-            if (data?.authenticated) {
-                go({to: "/dashboard", type: "replace"});
-            } else {
-                go({to: "/login", type: "replace"});
-            }
-        }
-    }, [isLoading, data]);
+  if (isLoading) {
+    return <>Checking Login Status</>;
+  }
 
-    if (isLoading) {
-        return <>Checking Login Status</> || null;
-    }
+  if (data?.authenticated) {
+    go({ to: "/dashboard", type: "replace" });
+  } else {
+    go({ to: "/login", type: "replace" });
+  }
 
-    return (<>Redirecting</>) || null;
+  return <>Redirecting</>;
 }
