1f69c9c1ccfefc47914be3b371a1046c303cddad
Derrick Hammer <derrick@derrickhammer.com>
2024-03-26 15:18:48 -0400

    style: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './sdk.js';
-export * from './account/generated/openapi.schemas.js';
-export * from './protocol/index.js';
-export { AccountError } from './account.js';
+export * from "./sdk.js";
+export * from "./account/generated/openapi.schemas.js";
+export * from "./protocol/index.js";
+export { AccountError } from "./account.js";
