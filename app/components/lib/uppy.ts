80f9fec610c2a92d74f91714e8ecf3b7b679eab0
Derrick Hammer <derrick@derrickhammer.com>
2024-03-28 22:52:44 -0400

    fix: temporarily disable parallelUploads

--- app/components/lib/uppy.ts
+++ app/components/lib/uppy.ts
@@ -170,7 +170,7 @@ export function useUppy() {
         });
 
         if (useTus) {
-          uppy.use(Tus, { limit: 6, parallelUploads: 10 });
+          uppy.use(Tus, { limit: 6, parallelUploads: 0 });
           uppy.addPreProcessor(tusPreprocessor);
         }
 
