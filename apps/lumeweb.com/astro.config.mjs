388958e07ca28baac05b40589fdbbf02912d01ff
Derrick Hammer <derrick@derrickhammer.com>
2024-04-10 21:28:34 -0400

    chore(docs.lumeweb.com): add nx config

--- apps/lumeweb.com/astro.config.mjs
+++ apps/lumeweb.com/astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lumeweb.com',
+    outDir: '../../dist/apps/lumeweb.com',
     integrations: [tailwind({ config: { applyBaseStyles: false } }), react(), sitemap()],
     experimental: {
         assets: true
