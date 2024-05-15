a458d759909562bc7862541bb9bc8b2da0e086b3
Derrick Hammer <derrick@derrickhammer.com>
2024-05-15 17:24:21 -0400

    feat(libs5-encoding): export modules

--- libs/libs5-encoding/src/index.ts
+++ libs/libs5-encoding/src/index.ts
@@ -1,1 +1,11 @@
 export * from "./bytes";
+export * from "./registry";
+export * from "./util";
+export * from "./types";
+
+export { KeyPairEd25519 } from "./ed25519";
+export { Multibase } from "./multibase";
+export { Multihash } from "./multihash";
+export { CID } from "./cid";
+export { EncryptedCID } from "./encrypted_cid";
+export { NodeId } from "./nodeId";
