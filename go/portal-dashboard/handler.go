4ef16ded299135ad44eb0d43a67c2db8286661f3
Derrick Hammer <derrick@derrickhammer.com>
2024-06-09 21:42:20 -0400

    fix(portal-dashboard): don't use a forward slash in fs.Sub

--- go/portal-dashboard/handler.go
+++ go/portal-dashboard/handler.go
@@ -10,7 +10,7 @@ import (
 var appFs embed.FS
 
 func Handler() http.Handler {
-	appFiles, _ := fs.Sub(appFs, "build/")
+	appFiles, _ := fs.Sub(appFs, "build")
 	appServ := http.FileServer(http.FS(appFiles))
 
 	return appServ
