eb810b1fa2bd1ff3f45e9b370df2676e20f76cdc
Juan Di Toro <ditorojuanf@gmail.com>
2024-06-23 00:20:05 +0200

    fix(portal-dashboard): more issues with react

--- apps/portal-dashboard/app/components/upload-file-button.tsx
+++ apps/portal-dashboard/app/components/upload-file-button.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { CloudUploadIcon } from './icons'
-import { Button } from './ui/button'
+import React from "react";
+import { CloudUploadIcon } from "./icons";
+import { Button } from "./ui/button";
 
-export const UploadFileButton = () => {
-    return (
-        <Button size={"lg"} className="w-full text-base font-semibold bg-upload-file-background hover:bg-current/60 text-gray-800 mt-10 lg:mt-4">
-            <CloudUploadIcon className="mr-1" />
-            Upload
-        </Button>
-    )
-}
+import { forwardRef } from "react";
+
+export const UploadFileButton = forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<typeof Button>
+>((props, ref) => {
+  return (
+    <Button
+      ref={ref}
+      size="lg"
+      className="w-full text-base font-semibold bg-upload-file-background hover:bg-current/60 text-gray-800 mt-10 lg:mt-4"
+      {...props}>
+      <CloudUploadIcon className="mr-1" />
+      Upload
+    </Button>
+  );
+});
+
+UploadFileButton.displayName = "UploadFileButton";
