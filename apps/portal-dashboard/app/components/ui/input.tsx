99a2c2d9b9d2fda6528976adb58ddab47df5841e
Eren <erencan_arica@hotmail.com>
2024-06-23 15:44:53 +0200

    chore(portal-dashboard): implement JSON table editor and add new UI components

--- apps/portal-dashboard/app/components/ui/input.tsx
+++ apps/portal-dashboard/app/components/ui/input.tsx
@@ -27,7 +27,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type && !mask ? type : "text"}
           className={cn(
-            "flex h-14 w-full rounded-md border border-input px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-input-placeholder focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-14 w-full bg-secondary rounded-md border border-input px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-input-placeholder focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
             className,
             leftIcon && "pl-10"
           )}
