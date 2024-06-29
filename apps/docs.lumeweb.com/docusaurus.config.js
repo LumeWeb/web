0804896560a2141fae6b399db8a1c128966acd17
Derrick Hammer <derrick@derrickhammer.com>
2024-06-28 20:29:19 -0400

    refactor(docs.lumeweb.com): upgrade docusaurus to v3

--- apps/docs.lumeweb.com/docusaurus.config.js
+++ apps/docs.lumeweb.com/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -98,8 +99,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Hammer Technologies LLC. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
       matomo: {
         matomoUrl: "https://piwiki.lumeweb.com/",
