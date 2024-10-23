b4f34ca3709f8bb5ca5e49951652b5e5113b9ff3
Juan Di Toro <ditorojuanf@gmail.com>
2024-10-23 12:34:05 +0200

    fix(portal-dashboard): using tabs for different parts of the billing

--- apps/portal-dashboard/app/routes/account/subscription.tsx
+++ apps/portal-dashboard/app/routes/account/subscription.tsx
@@ -1,4 +1,6 @@
+import { useSearchParams } from "@remix-run/react";
 import { Skeleton } from "portal-shared/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "portal-shared/components/ui/tabs";
 import useIsPaidBillingEnabled from "portal-shared/hooks/useIsPaidBillingEnabled";
 import useOnFreePlan from "portal-shared/hooks/useOnFreePlan";
 import React from "react";
@@ -19,13 +21,21 @@ export default function Subscription() {
     </SubscriptionProvider>
   );
 }
+const TABS = {
+  BILLING: "billing",
+  PAYMENT_HISTORY: "payment-history",
+  PAYMENT_METHOD: "payment-method",
+  ADDONS: "addons",
+} as const;
 
 function SubscriptionContent() {
   const { isLoading } = useSubscriptionContext();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const paidBillingEnabled = useIsPaidBillingEnabled();
 
   const onFreePlan = useOnFreePlan();
+  const searchTab = TABS[searchParams.get("tab") as keyof typeof TABS] ?? TABS.BILLING;
 
   if (isLoading) return <SkeletonSubscription />;
 
@@ -34,15 +44,39 @@ function SubscriptionContent() {
       <div className="lg:col-span-3">
         <PricingPlans />
       </div>
-      <div className="lg:col-span-3 border-t border-border/30 pt-4 flex flex-col gap-6">
-        {paidBillingEnabled && !onFreePlan && <PaymentHistory />}
-        {paidBillingEnabled && !onFreePlan && <ChangePaymentMethod />}
-        {false && <Addons />}
-      {paidBillingEnabled && (
-        <div className="lg:col-span-3">
-          <BillingInformation />
-        </div>
-      )}
+      <div className="lg:col-span-3 border-t border-border/30 pt-4">
+        <Tabs defaultValue={searchTab} onValueChange={(value) => setSearchParams({ tab: value })} className="space-y-4">
+          <TabsList>
+            {paidBillingEnabled && <TabsTrigger value="billing">Billing Information</TabsTrigger>}
+            {paidBillingEnabled && !onFreePlan && <TabsTrigger value="payment-history">Payment History</TabsTrigger>}
+            {paidBillingEnabled && !onFreePlan && <TabsTrigger value="payment-method">Payment Method</TabsTrigger>}
+            {false && <TabsTrigger value="addons">Add-ons</TabsTrigger>}
+          </TabsList>
+
+          {paidBillingEnabled && !onFreePlan && (
+            <TabsContent value="payment-history">
+              <PaymentHistory />
+            </TabsContent>
+          )}
+
+          {paidBillingEnabled && !onFreePlan && (
+            <TabsContent value="payment-method">
+              <ChangePaymentMethod />
+            </TabsContent>
+          )}
+
+          {paidBillingEnabled && (
+            <TabsContent value="billing">
+              <BillingInformation />
+            </TabsContent>
+          )}
+
+          {false && (
+            <TabsContent value="addons">
+              <Addons />
+            </TabsContent>
+          )}
+        </Tabs>
       </div>
     </div>
   );
