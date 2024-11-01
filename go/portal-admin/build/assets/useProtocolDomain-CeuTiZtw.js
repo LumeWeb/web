01066f7ee7e7279e31f8df95be8fc06981fa921c
Derrick Hammer <derrick@derrickhammer.com>
2024-11-01 04:48:22 -0400

    chore: reset app builds

--- go/portal-admin/build/assets/useProtocolDomain-CeuTiZtw.js
+++
@@ -1,8 +0,0 @@
-import { H as usePortalMeta } from "./createLucideIcon-DpDjZ8c4.js";
-function useProtocolDomain(proto) {
-  const portalMeta = usePortalMeta();
-  return `${proto}.${portalMeta == null ? void 0 : portalMeta.domain}`;
-}
-export {
-  useProtocolDomain as u
-};
