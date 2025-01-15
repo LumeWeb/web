bcd8f99b1db9290c09730af67d762a44f2a19414
Derrick Hammer <derrick@derrickhammer.com>
2025-01-14 22:56:29 -0500

    wip

--- apps/portal-dashboard/app/hooks/useSubscription.ts
+++ apps/portal-dashboard/app/hooks/useSubscription.ts
@@ -1,22 +1,15 @@
-import { CustomResponse, HttpError, useCustom } from "@refinedev/core";
+import { useCustom } from "@refinedev/core";
 import { SubscriptionResponse } from "portal-shared/dataProviders/accountProvider";
 import useApiUrl from "portal-shared/hooks/useApiUrl";
-import {
-  QueryObserverResult,
-  RefetchOptions,
-  RefetchQueryFilters,
-} from "@tanstack/react-query";
 
 export default function useSubscription() {
   const apiUrl = useApiUrl();
-  const {
-    data: subscriptionData
-  } = useCustom<SubscriptionResponse>({
+  const { data: subscriptionData } = useCustom<SubscriptionResponse>({
     url: `${apiUrl}/api/account/subscription`,
-    method: "get"
+    method: "get",
   });
 
   return {
-    subscriptionData: subscriptionData?.data
+    subscriptionData: subscriptionData?.data,
   };
 }
