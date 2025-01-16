3db24916d583d5056020d5b33013eeb04dba28f8
Derrick Hammer <derrick@derrickhammer.com>
2025-01-16 12:06:59 -0500

    wip

--- apps/portal-dashboard/app/features/subscription/contexts/SubscriptionContext.tsx
+++ apps/portal-dashboard/app/features/subscription/contexts/SubscriptionContext.tsx
@@ -114,7 +114,7 @@ export function SubscriptionProvider({ children }: SubscriptionProviderProps) {
       error,
       isLoading,
       plansAreLoading,
-      plansData?.data?.plans,
+      plansData?.data ?? [],
       selectedPlan,
       isProcessing,
       showPaymentDialog,
