4b9a9fe1479504aa77fe5b520ca397533df43f85
Derrick Hammer <derrick@derrickhammer.com>
2025-01-17 12:46:56 -0500

    wip

--- apps/portal-dashboard/app/features/subscription/components/subscription/SubscriptionManager.tsx
+++ apps/portal-dashboard/app/features/subscription/components/subscription/SubscriptionManager.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "@remix-run/react";
 import {
   SubscriptionProvider,
