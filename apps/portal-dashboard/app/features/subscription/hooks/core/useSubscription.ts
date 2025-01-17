6fa41e7a602b1602f4bb82d48f6a2f9b0a4b6037
Derrick Hammer <derrick@derrickhammer.com>
2025-01-16 21:42:30 -0500

    wip

---
+++ apps/portal-dashboard/app/features/subscription/hooks/core/useSubscription.ts
@@ -0,0 +1,17 @@
+import { useCustom } from "@refinedev/core";
+import { SubscriptionResponse } from "../../types/subscription.types";
+import useApiUrl from "portal-shared/hooks/useApiUrl";
+
+export function useSubscription() {
+  const apiUrl = useApiUrl();
+
+  const { data, isLoading } = useCustom<SubscriptionResponse>({
+    url: `${apiUrl}/api/account/subscription`,
+    method: "get",
+  });
+
+  return {
+    data,
+    isLoading,
+  };
+}
