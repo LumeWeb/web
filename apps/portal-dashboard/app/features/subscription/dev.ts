d7a0696ad9bdab046d79aad8088c066b6f9840e4
Derrick Hammer <derrick@derrickhammer.com>
2025-01-17 01:28:12 -0500

    wip

--- apps/portal-dashboard/app/features/subscription/dev.ts
+++ apps/portal-dashboard/app/features/subscription/dev.ts
@@ -1,10 +1,1 @@
-import { createDebugger, visualize } from 'robot3/debug';
-import { subscriptionMachine } from './machines/subscriptionMachine';
-
-if (process.env.NODE_ENV === 'development') {
-  const debug = createDebugger();
-  debug(subscriptionMachine);
-  
-  // Add state machine visualization
-  console.log('Subscription Machine States:', visualize(subscriptionMachine));
-}
+import "robot3/debug";
