c7756a4e2b03748d696c4e98d2e1f0f6d340cea4
Derrick Hammer <derrick@derrickhammer.com>
2025-07-17 05:16:24 -0400

    chore(go): update portal-plugin-dashboard build

---
+++ go/portal-plugin-dashboard/build/static/js/remoteEntry-D_yM9jQL.js
@@ -0,0 +1,3 @@
+import './core_dashboard__mf_v__runtimeInit__mf_v__-DlJLrLht.js';
+export { getExposes as get, init } from './virtual_mf-REMOTE_ENTRY_ID-2lXUVfFB.js';
+import './virtualExposes-DzIxNN4T.js';
