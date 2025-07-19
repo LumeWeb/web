ec73ad6c7609afb5a960bb16c05066283e334274
Derrick Hammer <derrick@derrickhammer.com>
2025-07-19 04:15:07 -0400

    chore(go): update portal-plugin-abuse build

--- go/portal-plugin-abuse/build/static/js/hostInit-znhRnf0-.js
+++ go/portal-plugin-abuse/build/static/js/hostInit-znhRnf0-.js
@@ -1,4 +1,4 @@
-const remoteEntryPromise = import('./remoteEntry-CLTyDJtT.js');
+const remoteEntryPromise = import('./remoteEntry-CeoOFZAo.js');
     // __tla only serves as a hack for vite-plugin-top-level-await. 
     Promise.resolve(remoteEntryPromise)
       .then(remoteEntry => {
