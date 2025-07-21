4eb4dda14f95bd817e7054678d28e7f27b216fb1
Derrick Hammer <derrick@derrickhammer.com>
2025-07-21 03:11:14 -0400

    chore(go): update portal-plugin-admin build

--- go/portal-plugin-admin/build/static/js/hostInit-OaxyLDZG.js
+++ go/portal-plugin-admin/build/static/js/hostInit-OaxyLDZG.js
@@ -1,4 +1,4 @@
-const remoteEntryPromise = import('./remoteEntry-B6iez3wa.js');
+const remoteEntryPromise = import('./remoteEntry-P5OVUOQu.js');
     // __tla only serves as a hack for vite-plugin-top-level-await. 
     Promise.resolve(remoteEntryPromise)
       .then(remoteEntry => {
--- go/portal-plugin-admin/build/static/js/remoteEntry-P5OVUOQu.js
+++ go/portal-plugin-admin/build/static/js/remoteEntry-P5OVUOQu.js
@@ -1,3 +1,3 @@
 import './core_admin__mf_v__runtimeInit__mf_v__-DfW_aMyq.js';
-export { getExposes as get, init } from './virtual_mf-REMOTE_ENTRY_ID-C6vYtfr8.js';
+export { getExposes as get, init } from './virtual_mf-REMOTE_ENTRY_ID-C0deFwAF.js';
 import './virtualExposes-DD5fkPrw.js';
