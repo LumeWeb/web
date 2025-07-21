9281cda6ea00e147275645169799ab29b82d19ec
Derrick Hammer <derrick@derrickhammer.com>
2025-07-21 02:18:51 -0400

    chore(go): update portal-dashboard build

---
+++ go/portal-dashboard/build/assets/hostInit-Bj6RvDGM.js
@@ -0,0 +1,6 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/remoteEntry-Qe1SpH_S.js","assets/dashboard__mf_v__runtimeInit__mf_v__-C0jw-Lkn.js","assets/virtualExposes-Dff6wIYf.js","assets/preload-helper-BkSzTOHT.js"])))=>i.map(i=>d[i]);
+import { _ as __vitePreload } from "./preload-helper-BkSzTOHT.js";
+const remoteEntryPromise = __vitePreload(() => import("./remoteEntry-Qe1SpH_S.js"), true ? __vite__mapDeps([0,1,2,3]) : void 0);
+Promise.resolve(remoteEntryPromise).then((remoteEntry) => {
+  return Promise.resolve(remoteEntry.__tla).then(remoteEntry.init).catch(remoteEntry.init);
+});
