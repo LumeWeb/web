1ee144e6e09b8a2cb43820196be30a9d380c1444
Derrick Hammer <derrick@derrickhammer.com>
2025-07-21 02:33:31 -0400

    chore(go): update portal-dashboard build

--- go/portal-dashboard/build/assets/index-CVY6YBRT.js
+++ go/portal-dashboard/build/assets/index-CVY6YBRT.js
@@ -30030,7 +30030,10 @@ function DesktopSidebar() {
       className: dashboard__loadShare___mf_0_lumeweb_mf_1_portal_mf_2_framework_mf_2_ui_mf_2_core__loadShare__.cn(
         "hidden md:flex",
         "flex flex-col fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
-        isCollapsed ? "w-[5%]" : "w-[10%]"
+        {
+          "sm:w-[20%] lg:w-[10%]": !isCollapsed,
+          "sm:w-[10%] lg:w-[5%]": isCollapsed
+        }
       ),
       children: [
         /* @__PURE__ */ jsxRuntimeExports.jsx(SidebarToggle, { setIsOpen: toggleCollapsed, isOpen: !isCollapsed }),
