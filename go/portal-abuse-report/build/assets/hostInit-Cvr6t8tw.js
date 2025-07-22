a1f39583bc2ffb5da851c02a597a3fd56351bb3b
Derrick Hammer <derrick@derrickhammer.com>
2025-07-22 08:10:33 -0400

    chore(go): update portal-abuse-report build

--- go/portal-abuse-report/build/assets/hostInit-Cvr6t8tw.js
+++
@@ -1,6 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/remoteEntry-Bebkd7RR.js","assets/abuse__mf_v__runtimeInit__mf_v__-D-IlhcC-.js","assets/virtualExposes-Dff6wIYf.js","assets/preload-helper-BkSzTOHT.js"])))=>i.map(i=>d[i]);
-import { _ as __vitePreload } from "./preload-helper-BkSzTOHT.js";
-const remoteEntryPromise = __vitePreload(() => import("./remoteEntry-Bebkd7RR.js"), true ? __vite__mapDeps([0,1,2,3]) : void 0);
-Promise.resolve(remoteEntryPromise).then((remoteEntry) => {
-  return Promise.resolve(remoteEntry.__tla).then(remoteEntry.init).catch(remoteEntry.init);
-});
