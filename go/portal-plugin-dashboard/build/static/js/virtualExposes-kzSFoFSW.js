19ce5aa9164a17d3a8bff7f70cef9af2368a0a1d
Derrick Hammer <derrick@derrickhammer.com>
2025-10-06 08:30:38 -0400

    chore(go): update dashboard plugin build

---
+++ go/portal-plugin-dashboard/build/static/js/virtualExposes-kzSFoFSW.js
@@ -0,0 +1,247 @@
+const exposesMap = {
+    
+        ".": async () => {
+          const importModule = await import('./plugin-CP00DieB.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./account/api-keys": async () => {
+          const importModule = await import('./account.api-keys-CXaua_ex.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./account/layout": async () => {
+          const importModule = await import('./account.layout-Bh70a_dQ.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./account/profile": async () => {
+          const importModule = await import('./account.profile-1ZpbyAwk.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./account/security": async () => {
+          const importModule = await import('./account.security-CZUtzw83.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./account/verify": async () => {
+          const importModule = await import('./account.verify-D_jSc_14.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./dashboard": async () => {
+          const importModule = await import('./dashboard-CyoVN0A2.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./index": async () => {
+          const importModule = await import('./index-Ck40T_pq.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./loginIndex": async () => {
+          const importModule = await import('./loginIndex-B9ov-e9t.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./loginOtp": async () => {
+          const importModule = await import('./otp-CD2EuG6c.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./registerIndex": async () => {
+          const importModule = await import('./registerIndex-e-dGZg9t.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./resetPassword/confirm": async () => {
+          const importModule = await import('./resetPassword.confirm-DeVKzqWw.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./resetPassword/layout": async () => {
+          const importModule = await import('./resetPassword.layout-CJp19GKz.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./resetPassword/reset": async () => {
+          const importModule = await import('./resetPassword.reset-BIj0lK72.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/2fa": async () => {
+          const importModule = await import('./2fa-TVJ8JSyQ.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/bio": async () => {
+          const importModule = await import('./bio-I_CkCA4V.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/delete": async () => {
+          const importModule = await import('./delete-Dq4Y4W_0.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/emailVerificationBanner": async () => {
+          const importModule = await import('./emailVerificationBanner-CdVXtlki.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/password": async () => {
+          const importModule = await import('./password-CEb_kQv7.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/account/profile": async () => {
+          const importModule = await import('./profile-CrM6W3Qr.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./widgets/upload/button": async () => {
+          const importModule = await import('./button-djbmzYgo.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      ,
+        "./operations": async () => {
+          const importModule = await import('./operations-D_PrQKbv.js');
+          const exportModule = {};
+          Object.assign(exportModule, importModule);
+          Object.defineProperty(exportModule, "__esModule", {
+            value: true,
+            enumerable: false
+          });
+          return exportModule
+        }
+      
+  };
+
+export { exposesMap as default };
