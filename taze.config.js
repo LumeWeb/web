b6949bc893a787ab03b760ab429413984e9fc88b
Derrick Hammer <derrick@derrickhammer.com>
2025-12-06 21:37:26 -0500

    chore: upgraded dependencies, added taze, remove dead patches

---
+++ taze.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "taze";
+
+export default defineConfig({
+  // ignore packages from bumping
+  exclude: ["vite", "tailwindcss"],
+  // fetch latest package info from registry without cache
+  force: true,
+  // write to package.json
+  write: true,
+  // run `npm install` or `yarn install` right after bumping
+  install: true,
+  // ignore paths for looking for package.json in monorepo
+  ignorePaths: ["**/node_modules/**", "**/test/**"],
+  // ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
+  ignoreOtherWorkspaces: true,
+  // override with different bumping mode for each package
+  // disable checking for "overrides" package.json field
+  depFields: {
+    overrides: false,
+  },
+});
