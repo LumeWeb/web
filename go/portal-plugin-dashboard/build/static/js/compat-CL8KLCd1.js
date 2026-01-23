02796832929de7f485e9ec5246c6e425a99b17c7
Derrick Hammer <derrick@derrickhammer.com>
2026-01-22 22:58:09 -0500

    Revert "chore: export app and plugin builds"
    This reverts commit 9174cbc03f003bed71b226081afd807232b43ffa.

--- go/portal-plugin-dashboard/build/static/js/compat-CL8KLCd1.js
+++
@@ -1,7 +0,0 @@
-// Zod 3 compat layer
-/** @deprecated Use the raw string literal codes instead, e.g. "invalid_type". */
-const ZodIssueCode = {
-    custom: "custom",
-};
-
-export { ZodIssueCode };
