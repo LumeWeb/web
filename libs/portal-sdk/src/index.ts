20adb5bee00fa42cfed07b06aa93cc72e7931642
Derrick Hammer <derrick@derrickhammer.com>
2026-06-01 15:46:18 +0000

    feat(portal-sdk): export poll, delay and PollOptions from http-utils
    Allows consumers to reuse the SDK's polling infrastructure instead
    of reimplementing interval/timeout defaults.

--- libs/portal-sdk/src/index.ts
+++ libs/portal-sdk/src/index.ts
@@ -2,6 +2,7 @@ export * from "@/sdk";
 export * from "@/types";
 export * from "@/openapi";
 export { DEFAULT_SETTLED_STATES, OPERATION_STATUS } from "@/account";
+export { delay, poll, type PollOptions } from "@/http-utils";
 export * from "@/account/generated/accountAPI.schemas";
 export * from '@/account/generated/default';
 export * from '@/query-utils';
